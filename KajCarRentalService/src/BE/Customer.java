ec9133bdbdcca4a5e188319a7529ba61a0310240
kriahja <kriahja@gmail.com>
2015-03-25 18:34:06 +0100

    Added Packages and Customer class in BE

--- KajCarRentalService/src/BE/Customer.java
+++ KajCarRentalService/src/BE/Customer.java
@@ -3,21 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package kajcarrentalservice;
+package BE;
 
 /**
  *
  * @author notandi
  */
-public class KajCarRentalService
+public class Customer
 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
-    {
-        // TODO code application logic here
-    }
     
 }
