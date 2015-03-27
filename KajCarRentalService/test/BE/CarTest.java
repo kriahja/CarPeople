6daf710f9bd8d2913bebd1cb0e319a4140bd8208
kriahja <kriahja@gmail.com>
2015-03-27 12:23:07 +0100

    carTest initialized

---
+++ KajCarRentalService/test/BE/CarTest.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BE;
+
+import org.junit.Test;
+import org.junit.Before;
+
+/**
+ *
+ * @author notandi
+ */
+public class CarTest
+{
+    
+    @Before
+    public void test()
+    {
+        
+    }
+
+    /**
+     * Test of getName method, of class Car.
+     */
+    @Test
+    public void testGetName()
+    {
+    }
+
+    /**
+     * Test of setName method, of class Car.
+     */
+    @Test
+    public void testSetName()
+    {
+    }
+
+    /**
+     * Test of getDepId method, of class Car.
+     */
+    @Test
+    public void testGetDepId()
+    {
+    }
+
+    /**
+     * Test of getCatId method, of class Car.
+     */
+    @Test
+    public void testGetCatId()
+    {
+    }
+
+    /**
+     * Test of getKm method, of class Car.
+     */
+    @Test
+    public void testGetKm()
+    {
+    }
+
+    /**
+     * Test of setKm method, of class Car.
+     */
+    @Test
+    public void testSetKm()
+    {
+    }
+
+    /**
+     * Test of isIsDamaged method, of class Car.
+     */
+    @Test
+    public void testIsIsDamaged()
+    {
+    }
+
+    /**
+     * Test of setIsDamaged method, of class Car.
+     */
+    @Test
+    public void testSetIsDamaged()
+    {
+    }
+
+    /**
+     * Test of isIsFull method, of class Car.
+     */
+    @Test
+    public void testIsIsFull()
+    {
+    }
+
+    /**
+     * Test of setIsFull method, of class Car.
+     */
+    @Test
+    public void testSetIsFull()
+    {
+    }
+
+    /**
+     * Test of isIsFixed method, of class Car.
+     */
+    @Test
+    public void testIsIsFixed()
+    {
+    }
+
+    /**
+     * Test of setIsFixed method, of class Car.
+     */
+    @Test
+    public void testSetIsFixed()
+    {
+    }
+
+    /**
+     * Test of toString method, of class Car.
+     */
+    @Test
+    public void testToString()
+    {
+    }
+    
+}
