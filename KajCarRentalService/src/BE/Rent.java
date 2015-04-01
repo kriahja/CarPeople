aafac26bb4c14f904157a2b4419e124f771ff65e
ZALI <ZALI@WIN-NG29UH56TQE.lan>
2015-04-01 19:25:40 +0200

    Added Rent

---
+++ KajCarRentalService/src/BE/Rent.java
@@ -0,0 +1,30 @@
+
+package BE;
+
+/**
+ *
+ * @author ZALI
+ */
+public class Rent {
+    public int startDate;
+    public int endDate;
+
+    public int getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(int startDate) {
+        this.startDate = startDate;
+    }
+
+    public int getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(int endDate) {
+        this.endDate = endDate;
+    }
+    
+    
+    
+}
