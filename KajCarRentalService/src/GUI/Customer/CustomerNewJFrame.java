6c85c18746299d2ebc3d5462e79cc64c71db0680
kriahja <kriahja@gmail.com>
2015-04-05 16:06:53 +0200

    GUI - WindowCtrl  - Added

--- KajCarRentalService/src/GUI/Customer/CustomerNewJFrame.java
+++ KajCarRentalService/src/GUI/Customer/CustomerNewJFrame.java
@@ -404,76 +404,30 @@ public CustomerNewJFrame()
         addCredit.setVisible(true);
     }//GEN-LAST:event_btnAddCreditCardActionPerformed
 
-    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {                                        
        
-    }//GEN-LAST:event_btnBackActionPerformed
+    }                                       
 
-    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {                                        
        
-    }//GEN-LAST:event_btnBackActionPerformed
+    }                                       
 
-    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {                                        
        
-    }//GEN-LAST:event_btnBackActionPerformed
+    }                                       
 
-    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {                                        
        
-    }//GEN-LAST:event_btnBackActionPerformed
+    }                                       
+
+    private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {                                        
+       
+    }                                       
 
     private void btnBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBackActionPerformed
 
     }//GEN-LAST:event_btnBackActionPerformed
 
-/**
- * @param args the command line arguments
- */
-public static void main(String args[])
-{
-    /* Set the Nimbus look and feel */
-    //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-     * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-     */
-    try
-    {
-        for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
-        {
-            if ("Nimbus".equals(info.getName()))
-            {
-                javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                break;
-            }
-        }
-    }
-    catch (ClassNotFoundException ex)
-    {
-        java.util.logging.Logger.getLogger(CustomerNewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    }
-    catch (InstantiationException ex)
-    {
-        java.util.logging.Logger.getLogger(CustomerNewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    }
-    catch (IllegalAccessException ex)
-    {
-        java.util.logging.Logger.getLogger(CustomerNewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    }
-    catch (javax.swing.UnsupportedLookAndFeelException ex)
-    {
-        java.util.logging.Logger.getLogger(CustomerNewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-    }
-    //</editor-fold>
-    //</editor-fold>
-    //</editor-fold>
-    //</editor-fold>
-
-    /* Create and display the form */
-    java.awt.EventQueue.invokeLater(new Runnable()
-    {
-    public void run()
-    {
-        new CustomerNewJFrame().setVisible(true);
-    }
-    });
 }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
